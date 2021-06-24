fd094ca2212ae09e74a9c444df31d730e1823fa9
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-24 18:56:11 -0400

    first commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname,'../index.html'));
+    res.sendFile(path.join(__dirname,'public/index.html'));
 })
 
 const port= process.env.PORT||4000
