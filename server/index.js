edb372dc4c2c9e24c2dd0ec5aea0d983fdaee114
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-24 15:56:05 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'../index.html'));
+})
+
+const port= process.env.PORT||4000
+
+app.listen(port,function(){
+    console.log(`server is running on ${port}`)
+})
